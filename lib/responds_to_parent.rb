ee8a8b08d8254ed1ffd98e55a65cb3ae63c621ad
Kennon Ballou <kennon@angryturnip.com>
2011-04-19 16:02:58 +0200

    Added missing ActionController::Redirecting#erase_redirect_results which was removed from Rails3

--- lib/responds_to_parent.rb
+++ lib/responds_to_parent.rb
@@ -2,6 +2,19 @@ require 'responds_to_parent/action_controller'
 require 'responds_to_parent/selector_assertion'
 
 module ActionController
+  module Redirecting
+    def erase_redirect_results #:nodoc:
+      self.status = 200 #302 #DEFAULT_RENDER_STATUS_CODE
+      response.headers.delete('Location')
+    end
+    
+    def erase_results
+      @_response_body = nil
+    end
+  end
+end
+
+module ActionController
   class Base
     include RespondsToParent::ActionController
   end
